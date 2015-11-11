a9592a89c7fe9092101d6be400818f1f8348322f
Paul Montgomery (r621362) <paul.montgomery@cambiahealth.com>
2015-11-11 05:55:22 -0800

    updated Game.js

--- Game.js
+++ Game.js
@@ -8,7 +8,9 @@ var NeuralNet = function(initial_inputs, layer_count, depth_count) {
 	this.matrix[0]["inputs"] = this.buildInputs( initial_inputs.length )
 	this.matrix[0]["weights"] = this.buildWeights( initial_inputs.length, depth_count );
 	this.matrix[0]["nodes"] = this.buildNodes( depth_count );
-
+	for ( var i in initial_inputs ) {
+		this.matrix[0]["inputs"][i] = initial_inputs[i];
+	}
 	//ok, now for the main show... start i off at '1'
 	for ( var i = 1; i < layer_count; i++ ) {
 		this.matrix[i] = {};
@@ -33,9 +35,9 @@ NeuralNet.prototype = {
 				if ( this.matrix[layer]["nodes"][i] !== undefined ) {
 					out += " node: " + this.matrix[layer]["nodes"][i].toFixed(3) + "\t";
 				} else {
-					out += " node: --.--- ";
+					//out += " node: --.--- ";
+					out += " node:        ";
 				}
-
 				if ( this.matrix[layer]["inputs"][i] !== undefined ) {
 
 					out += "input: " + this.matrix[layer]["inputs"][i].toFixed(3) + "\t|\t";
@@ -80,8 +82,8 @@ NeuralNet.prototype = {
 
 var UnitTest = function(){
 	var ary = [0.05, 0.1]; 
-	var layers = 2;
-	var depth = 4;
+	var layers =  2;
+	var depth = 2;
 	this.NN = new NeuralNet( ary,layers,depth);
 	this.setup();
 	this.NN.display();
@@ -89,6 +91,27 @@ var UnitTest = function(){
 
 UnitTest.prototype = {
 	setup : function() {
+
+		// set the weights to permit testing the results
+		// with known inputs
+
+		// layer 1, node 1
+		this.NN.matrix[0]["weights"][0][0] = .15;
+		this.NN.matrix[0]["weights"][1][0] = .2;
+		this.NN.matrix[0]["weights"][2][0] = .35;
+		// layer 1, node 2
+		this.NN.matrix[0]["weights"][0][1] = .2;
+		this.NN.matrix[0]["weights"][1][1] = .3;
+		this.NN.matrix[0]["weights"][2][1] = .35;
+		// layer 2, node 1
+		this.NN.matrix[1]["weights"][0][0] = .4;
+		this.NN.matrix[1]["weights"][1][0] = .5;
+		this.NN.matrix[1]["weights"][2][0] = .6;
+		// layer 2, node 2
+		this.NN.matrix[1]["weights"][0][1] = .45;
+		this.NN.matrix[1]["weights"][1][1] = .55;
+		this.NN.matrix[1]["weights"][2][1] = .6;
+		
 
 	}
 }
